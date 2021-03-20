b4ec715df0b361f3cddcc966ebb48b2bb3677658
mongamae-nioh <jkawaoka@gmail.com>
2021-03-20 10:06:28 +0900

    2nd commit

--- chapter-2/src/index.ts
+++ chapter-2/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, typescript!')
+let a = 1 + 2
+let b = a + 3
+let c = {
+    apple: a,
+    banana: b
+}
+let d = c.apple * 4
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
